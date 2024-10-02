268f6ff5148c72311244ee3f1b138015d335c1ce
Dani <dimeny.dani@gmail.com>
2024-10-02 13:52:09 +0200

    Learned about for and while loops.

--- app.py
+++ app.py
@@ -9,3 +9,21 @@ elif b:
     print("b is True")
 else:
     print("Neither a or b is True") """
+
+# What is for loop
+"""
+for i in range(5):
+    print(i) """
+
+# while
+
+""" message: str = "Give me your answer!"
+
+while True:
+    answer: str = input(message)
+    if len(answer) >= 3:
+        print("Thank you!")
+        # Break out of the while loop
+        break
+    else:
+        message = ("Please give me at least 3 characters:") """
